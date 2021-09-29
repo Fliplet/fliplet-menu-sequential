99fb38134d14d0941db241f7edf396fe8969a446
OleksandrLavrynenko <oleksandr.lav@upplabs.com>
2021-09-29 10:23:13 +0300

    [FIX] fix translatin

--- js/menu.js
+++ js/menu.js
@@ -19,7 +19,7 @@ function init() {
       '<i class="fa fa-fw fa-sign-out"></i>',
       '</div>',
       '<i class="fa fa-angle-right linked-icon" aria-hidden="true"></i>',
-      '<span class="internal-link buttonControl">' + T('widgets.menuSequential.dataSource.actions.exit') + '</span>',
+      '<span class="internal-link buttonControl">' + T('widgets.menu.sequentialBar.actions.exit') + '</span>',
       '</li>'
     ].join(''));
 
